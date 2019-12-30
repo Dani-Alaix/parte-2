efd28c2e87eafb32b93deb9a4b6506d9bf848b5b
daniela <dani_6352@hotmail.com>
2019-12-29 22:06:00 -0500

    menu

--- code.js
+++ code.js
@@ -1,14 +1,13 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    console.log(document.getElementById("myDropdown"));
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
+      console.log(dropdowns);
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
         if (openDropdown.classList.contains('show')) {
